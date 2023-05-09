0c8993dabe2ef695b2d3ac3c8bfeba3392203259
xharf <123190123@student.upnyk.ac.id>
2023-05-10 00:38:06 +0700

    finish #1 project

---
+++ model/axios.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const Axios = axios.create({
+	baseURL: "http://api.weatherapi.com/v1",
+});
+
+// Add a request interceptor
+Axios.interceptors.request.use(
+	function (config) {
+		return config;
+	},
+	function (error) {
+		// Do something with request error
+		return Promise.reject(error);
+	},
+);
+
+// Add a response interceptor
+Axios.interceptors.response.use(
+	function (response) {
+		// Any status code that lie within the range of 2xx cause this function to trigger
+		// Do something with response data
+		return response;
+	},
+	function (error) {
+		// Any status codes that falls outside the range of 2xx cause this function to trigger
+		// Do something with response error
+		return Promise.reject(error);
+	},
+);
+
+export default Axios;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import "@/styles/Home.css";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
